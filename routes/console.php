434fbccd115acf338523ca6fcd24dfd0424d16f7
Lucasion <lucasion@hotmail.com>
2026-05-14 01:26:44 +0200

    Small console clean up

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,7 @@
 <?php
 
 use App\Models\User;
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
 
 Artisan::command('shup:expired', function () {
     $this->comment('Deleting expired items');
